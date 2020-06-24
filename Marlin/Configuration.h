a36a6fbd1fc051a833c38a23b232ca1b1387eb05
Shizzy713 <sbickerstaff@gmail.com>
2020-06-23 22:41:13 -0500

    BOARD_BTT_SKR_MINI_E3_V1_2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Shizzy713, Ender_3, BTT_SKR_mini_E3_V2_0, TH3D EZABL)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Shizzy713, Ender_3, BTT_SKR_mini_E3_V1_2, TH3D EZABL)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -128,7 +128,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V2_0
+  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V1_2
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
