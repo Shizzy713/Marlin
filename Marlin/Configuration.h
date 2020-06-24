5ef0f59901dd2990294134cc1e96094747082369
Shizzy713 <sbickerstaff@gmail.com>
2020-06-23 21:27:54 -0500

    Machine Name Update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Basic"
+#define CUSTOM_MACHINE_NAME "Basic Bitch"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
