7f2c7766a9f591d449d32b0ff47f22c840eb5697
Shizzy713 <sbickerstaff@gmail.com>
2020-07-03 05:42:44 -0500

    update version

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Basic Bitch 1.2"
+#define CUSTOM_MACHINE_NAME "Ender 3 V1.3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
