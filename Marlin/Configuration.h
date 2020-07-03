b907c2df83df42e7fd27783346dd82224bad5632
Shizzy713 <sbickerstaff@gmail.com>
2020-07-03 18:01:53 -0500

    update controller fan & version number

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3 V1.4"
+#define CUSTOM_MACHINE_NAME "Ender 3 V1.4" // M115 to confirm current version
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
