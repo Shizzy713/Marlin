0e7efd5b49e4c92353e4229778778517e45b6b3d
Shizzy713 <sbickerstaff@gmail.com>
2020-08-22 05:19:42 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Marlin V 2.0.6.1" // M115 to confirm current version
+#define CUSTOM_MACHINE_NAME "2.0.6.1" // M115 to confirm current version
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
